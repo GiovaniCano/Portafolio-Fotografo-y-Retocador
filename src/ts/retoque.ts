23bd87ca7d93063c66ea7c7c24096c6f5711f803
Giovani <giovanicnvg@gmail.com>
2021-12-18 17:41:10 -0600

    Base layout done. Modernizer moved to build. Style adjustements in globals, variables and header. Before-after slider done. Added icons (no functions yet).

--- src/ts/retoque.ts
+++ src/ts/retoque.ts
@@ -1,5 +1,36 @@
 function retoque() {
     if(!document.getElementById("body-retoque")) return;
 
-    
+    const sliders = document.querySelectorAll(".slider-input")
+    sliders.forEach(slider=>{
+        slider.addEventListener("input", function(e){
+            //@ts-ignore
+            const line = e.target.parentNode.children[2]
+            //@ts-ignore
+            const before = e.target.parentNode.children[1]
+            //@ts-ignore
+            const value = e.target.value + "%"
+
+            line.style.left = value
+            before.style.width = value
+        })
+        slider.addEventListener("mousedown", function(e){
+            //@ts-ignore
+            const circle = e.target.parentNode.children[2].children[1]
+            circle.classList.add("transparent")
+
+            //@ts-ignore
+            const line = e.target.parentNode.children[2].children[0]
+            line.classList.add("mousedown")
+        })
+        slider.addEventListener("mouseup", function(e){
+            //@ts-ignore
+            const circle = e.target.parentNode.children[2].children[1]
+            circle.classList.remove("transparent")
+
+            //@ts-ignore
+            const line = e.target.parentNode.children[2].children[0]
+            line.classList.remove("mousedown")
+        })
+    })
 }
