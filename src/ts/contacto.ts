242f2a42b4195ad0f56a89e4269c09643faee195
Giovani <giovanicnvg@gmail.com>
2021-12-26 23:54:59 -0600

    Layout finished. Responsive. Improved color for header. Added css-class button.

---
+++ src/ts/contacto.ts
@@ -0,0 +1,11 @@
+function contacto() {    
+    if(document.body.id !== "body-contacto") return;
+
+    //evento submit aqui
+
+        /* validacion */
+
+        /* enviar a formsubmit por fetch api */
+
+        //mensajes de envio exitoso o fallido
+}
